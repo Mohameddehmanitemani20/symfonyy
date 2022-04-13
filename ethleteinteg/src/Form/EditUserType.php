b1e4e5d208e5554c766165ad78b95e51420e226e
mohamed dehmani temani <61525035+Mohameddehmanitemani20@users.noreply.github.com>
2022-04-13 01:06:12 +0100

    ..

---
+++ ethleteinteg/src/Form/EditUserType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EditUserType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+        ->add('adresse')
+        ->add('dateNaissance')
+        ->add('email')
+        ->add('nom')
+        ->add('numTel')
+        ->add('password')
+        ->add('prenom')
+        ->add('roles')
+        ->add('username')
+        ->add('genre')
+        ->add('idEq')
+            
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
